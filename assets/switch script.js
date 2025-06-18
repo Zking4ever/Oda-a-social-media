d55e319d6fa4f3a0fbe24e336fdb4337a86aeb5d
Astawus <yanosastawus@gmail.com>
2025-06-18 10:09:39 +0300

    signUp logIn switch animation added

--- assets/switch script.js
+++ assets/switch script.js
@@ -2,17 +2,26 @@ var to_sign_up = document.getElementById("to_sign_up");
 var to_log_in = document.getElementById("to_log_in");
 
 var part1 = document.getElementsByClassName('part1')[0];
+var part2 = document.getElementsByClassName('part2')[0];
 var part3 = document.getElementsByClassName('part3')[0];
-
-to_log_in.onclick = function(){
-    part3.style.display ="flex";
-    part1.style.display ="none";
+var temp;
+function delay(t){
+    return new Promise(response=>setTimeout(response,t));
+}
+async function logIn(){
+    part1.style.transform ="translateX(133%)";
+    part2.style.transform ="translateX(-75%)";
+    await delay(500);
+    temp = part1.innerHTML;
+    part1.innerHTML = part3.innerHTML;
 }
 
-to_sign_up.onclick = function(){
-    part1.style.display ="flex";
-    part3.style.display ="none";
-}
+async function signUp(){
+    part1.style.transform ="translateX(0)";
+    part2.style.transform ="translateX(0)";
+    await delay(500);
+    part1.innerHTML = temp;
+};
 
 var email = document.getElementById("email");
 var email_lable = document.getElementById("email_lable");
