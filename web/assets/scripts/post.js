e41e8bb6f3920db552144192358732ed8bbd1dab
Astawus <yanosastawus@gmail.com>
2025-07-24 01:09:41 +0300

    post fetching when reaching the bottom with loading animation and post filter to show a new one when loading

---
+++ web/assets/scripts/post.js
@@ -0,0 +1,168 @@
+radios[0].click();
+//everytime the page starts with home
+
+var isLoading=false;
+
+var post = document.getElementsByClassName('post');
+
+loaded_post.addEventListener('scroll',function(){
+    
+    Loader = post[post.length-1];
+    if(!Loader || Loader.id != "postLOADER"){
+        return;
+    }
+    if(window.getComputedStyle(Loader).opacity !=1){
+        if(!isLoading){
+            isLoading=true;
+            fetch_post();
+        }
+    }
+});
+
+function fetch_post(){
+        Loader = post[post.length-1];
+        var xml = new XMLHttpRequest;
+        xml.onload = function(){ 
+            if(xml.readyState==4 || xml.status==200){
+                var response = JSON.parse(xml.response);
+                if( (response['posts'].split('NO NEW POST')).length ==2){ 
+                    // the response is no new post 
+                    if(post[post.length-2].textContent == '  NO NEW POST.. '){
+                        //and already it has been displayed
+                        isLoading=false; //for next time loading
+                        return;
+                   }
+                }else if(post[post.length-2].textContent == '  NO NEW POST.. '){
+                    loaded_post.removeChild(post[post.length-2]);
+                }
+                console.log(post[post.length-2].textContent )
+                loaded_post.removeChild(Loader);
+                loaded_post.innerHTML += response['posts'];
+                isLoading=false;
+            }
+        }
+        xml.open("GET","backend/api.php?request_type=fetch_post",true);
+        xml.send();
+}
+
+var seenPost = [];
+
+function Post_seen(e){
+    var element = e.target;
+    while(element.className != 'post'){
+        element = element.parentElement;
+    }
+    var postid = element.getElementsByClassName("react")[0].id;
+
+    if(seenPost.includes(postid)){
+        return;
+    }
+    var form = new FormData;
+    var xml = new XMLHttpRequest;
+    xml.onload = function (){
+        seenPost.push(postid);
+    }
+    form.append("request_type","post_reaction");
+    form.append("data_type","post_seen");
+    form.append("postid",postid);
+
+    xml.open("POST","backend/api.php",true);
+    xml.send(form);
+}
+
+function reactPost(event,num){
+    var element = event.target;
+    if(!element.id){
+        element = element.parentElement;
+        if(!element.id){
+            element = element.parentElement;
+        }
+    }
+    var span = element.getElementsByTagName('span')[0];
+    var postid  = element.id;
+    var number  = span.innerHTML;
+    var react_type = (num==1 ? "likes" : "reposts");
+    var status;
+
+    if(element.className == 'react reacted'){
+        //this time user is removing the 
+        element.className = 'react';
+        number--;
+        status = "removing";
+    }else{
+        //user reacting
+        element.className = 'react reacted';
+        number++;
+        status = "reacting";
+    }
+
+    var form = new FormData;
+    var xml = new XMLHttpRequest;
+    xml.onload = function(){
+        if(xml.readyState==4 || xml.status==200){
+            if(xml.response == "saved to db"){
+                span.innerHTML = number;
+            }
+        }
+    }
+
+    form.append("request_type","post_reaction");
+    form.append("data_type","post_reaction");
+    form.append("react_type",react_type);
+    form.append("postid",postid);
+    form.append("number",number);
+    form.append("status",status);
+
+    xml.open("POST","backend/api.php",true);
+    xml.send(form);
+
+}
+
+function postComments(e){
+    var element = e.target;
+    if(!element.id){
+        element = element.parentElement;
+        if(!element.id){
+            element = element.parentElement;
+        }
+    }
+    var span = element.getElementsByTagName('span')[0];
+    var postid  = element.id;
+    var number  = span.innerHTML;
+        var commentContainer = document.getElementsByClassName("commentContainer")[0];
+        commentContainer.style.display = "block";
+        readPostComment(postid);
+        //preparing for writing comment
+            type_inputes.style.display = "flex";
+            type_inputes.style.position = "absolute";
+            type_inputes.getElementsByTagName("label")[0].style.display = "none";
+            type_inputes.getElementsByTagName("input")[1].placeholder = "comment on this post";
+            type_inputes.getElementsByTagName("input")[1].setAttribute('prev_com_no',number);
+            type_inputes.getElementsByTagName("input")[1].setAttribute('comment_this_post',postid);
+}
+
+function readPostComment(postid){
+    
+        var commentContainer = document.getElementsByClassName("commentContainer")[0];
+        //commentContainer.innerHTML = "post comment on "+postid+" goes here";
+        var form = new FormData;
+        var xml= new XMLHttpRequest;
+        xml.onload = function(){
+            if(xml.readyState==4 || xml.status==200){
+                commentContainer.innerHTML = xml.response;
+            }
+        }
+        form.append("request_type","post_reaction");
+        form.append("data_type","read_comment");
+        form.append("postid",postid);
+
+        xml.open("POST","backend/api.php",true);
+        xml.send(form);
+}
+function clothPostComment(e){
+        e.target.parentElement.style.display = "none";
+        type_inputes.getElementsByTagName("label")[0].style.display = "block";
+        type_inputes.style.display = "none";
+        type_inputes.getElementsByTagName("input")[1].removeAttribute('comment_this_post');
+        type_inputes.getElementsByTagName("input")[1].removeAttribute('prev_com_no');
+}
--- web/backend/includes/fetch_posts.php
+++ web/backend/includes/fetch_posts.php
@@ -16,62 +16,76 @@ if($excute->num_rows>0){
     }
     $valuesForQuery .= ')';
 
+$fetchedPost = [];
 
-$query = "SELECT * from posts";
+$query = "SELECT postid from posts";
 $result = $conn->query($query);
 
-$post = "";
 if($result->num_rows>0){
     while($row = $result->fetch_assoc()){
-        
+        array_push($fetchedPost,$row['postid']);
+    }
+}
+
+$response = array_diff_assoc($fetchedPost,$seenPosts);
+
+$post = "";
+
+if(count($response)>0){
+    foreach ($response as $key => $value) {
+        # code...
+        $query = "SELECT * from posts where postid = '$value' ";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+
         #here another query to find out the sender
-        $queryForSenders = "SELECT userid,name,userid,username,source from users where userid = '$row[sender]' ";
-        
-        $sender = $conn->query($queryForSenders)->fetch_assoc();
-                    $post.="<div class='post' >
-                        <div class='sender' id=".$sender['userid']." onclick='get_profile(event)'>
-                            <img src='backend/".$sender['source']."'>
-                            <div class='detail'>
-                                <h3 style='position:absolute;transform: translateY(-7px);'>".$sender['name']."</h3>
-                                <span style='font-size:12px;margin-left:7px;position:absolute;transform: translateY(10px);'>@".$sender['username']."</span>
+            $queryForSenders = "SELECT userid,name,userid,username,source from users where userid = '$row[sender]' ";
+            
+            $sender = $conn->query($queryForSenders)->fetch_assoc();
+                        $post.="<div class='post' onmousewheel='Post_seen(event)'>
+                            <div class='sender' id=".$sender['userid']." onclick='get_profile(event)'>
+                                <img src='backend/".$sender['source']."'>
+                                <div class='detail'>
+                                    <h3 style='position:absolute;transform: translateY(-7px);'>".$sender['name']."</h3>
+                                    <span style='font-size:12px;margin-left:7px;position:absolute;transform: translateY(10px);'>@".$sender['username']."</span>
+                                </div>
                             </div>
-                        </div>
-                        <div class='img_container'>
-                            <img src='backend/".$row['source']."'>
-                        </div>
-                        <div class='reactions'>";
+                            <div class='img_container'>
+                                <img src='backend/".$row['source']."'>
+                            </div>
+                            <div class='reactions'>";
 
-                        $queryforReaction = "SELECT * from postreaction where userid = '$userid' and postid = '$row[postid]' ";
-                        $reactionStatus = $conn->query($queryforReaction)->fetch_assoc();
+                            $queryforReaction = "SELECT * from postreaction where userid = '$userid' and postid = '$row[postid]' ";
+                            $reactionStatus = $conn->query($queryforReaction)->fetch_assoc();
 
-                        $post.="<div ".(!empty($reactionStatus) && $reactionStatus['liked']==1? "class='react reacted' " : "class='react' "). " id=".$row['postid']." onclick='reactPost(event,1)'>
-                                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-heart' viewBox='0 0 16 16'>
-                                    <path d='m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15'/>
-                                    <path class='filled' fill-rule='evenodd' d='M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314'/>
-                                </svg>
-                                <span>".$row['likes']."</span>
+                            $post.="<div ".(!empty($reactionStatus) && $reactionStatus['liked']==1? "class='react reacted' " : "class='react' "). " id=".$row['postid']." onclick='reactPost(event,1)'>
+                                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-heart' viewBox='0 0 16 16'>
+                                        <path d='m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15'/>
+                                        <path class='filled' fill-rule='evenodd' d='M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314'/>
+                                    </svg>
+                                    <span>".$row['likes']."</span>
+                                </div>
+                                <div ".(!empty($reactionStatus) && $reactionStatus['commented']==1? "class='react reacted' " : "class='react' "). " id=".$row['postid']." onclick='postComments(event)'>
+                                    <svg  width='16' height='16' fill='currentColor' class='bi bi-chat-left-dots' viewBox='0 0 16 16'>
+                                        <path d='M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z'/>
+                                        <path d='M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0'/>
+                                    </svg>
+                                    <span>".$row['comments']."</span>
+                                </div>
+                                <div ".(!empty($reactionStatus) && $reactionStatus['reposted']==1? "class='react reacted' " : "class='react' "). " id=".$row['postid']." onclick='reactPost(event,2)'>
+                                    <svg width='16' height='16' fill='currentColor' class='bi bi-repeat' viewBox='0 0 16 16'>
+                                        <path d='M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z'/>
+                                    </svg>
+                                    <span>".$row['reposts']."</span>
+                                </div>
                             </div>
-                            <div ".(!empty($reactionStatus) && $reactionStatus['commented']==1? "class='react reacted' " : "class='react' "). " id=".$row['postid']." onclick='postComments(event)'>
-                                <svg  width='16' height='16' fill='currentColor' class='bi bi-chat-left-dots' viewBox='0 0 16 16'>
-                                    <path d='M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z'/>
-                                    <path d='M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0'/>
-                                </svg>
-                                <span>".$row['comments']."</span>
-                            </div>
-                            <div ".(!empty($reactionStatus) && $reactionStatus['reposted']==1? "class='react reacted' " : "class='react' "). " id=".$row['postid']." onclick='reactPost(event,2)'>
-                                <svg width='16' height='16' fill='currentColor' class='bi bi-repeat' viewBox='0 0 16 16'>
-                                    <path d='M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z'/>
-                                </svg>
-                                <span>".$row['reposts']."</span>
-                            </div>
-                        </div>
-                        <span class='caption'>".$row['caption']."</span>
-                    </div>";
-        }
+                            <span class='caption'>".$row['caption']."</span>
+                        </div>";
+    }
 }else{
    $post.="<center class='post' style='border:none;'> <div> NO NEW POST..</div> </center>";
-
 }
+
 $post.="<center class='post' style='border:none;' id='postLOADER'> <div style='transform-origin:50% 50%;margin:10px;' class='loading'></div> </center>";
 
 $data['posts']= $post;
