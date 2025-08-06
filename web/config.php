42f89a16cf86e853683b1b2415a0556b7f9325af
Astawus <yanosastawus@gmail.com>
2025-08-06 14:36:41 +0300

    added prepared statement for security of db

---
+++ web/config.php
@@ -0,0 +1,21 @@
+<?php
+
+require "backend/conn.php";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$stmt = $conn->prepare('SELECT name,password from admin where email = ?');
+$stmt->bind_param('s',$email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+
+if($row = $result->fetch_assoc()){
+    if(password_verify($password, $row['password'])){
+        session_start();
+        $_SESSION['user'] = $row['name'];
+        include "admin.php";
+    }
+}
+
--- web/sign.php
+++ web/sign.php
@@ -8,24 +8,37 @@ if(isset($_POST) && !empty($_POST) && $_POST['type']=="log_in" ){
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $checkForUser = "SELECT userid from users where email = '$email' && password = '$password'";
-    $checkUserResponse = $conn->query($checkForUser);
+    $stmt = $conn->prepare('SELECT userid,password from users where email = ? ');
+    $stmt->bind_param("s",$email);
+    $stmt->execute();
+    $stmt->store_result();
 
-    
-    if($checkUserResponse->num_rows>0){
-        $userdata = $checkUserResponse->fetch_assoc();
-        $data['result'] = "log in";
-        $data['status'] = "great";
-        $_SESSION['userid'] = $userdata['userid'];
+    if($stmt->num_rows>0){
+        $stmt->bind_result($userid, $Storedpassword);
+        if($stmt->fetch()){
+            if(password_verify($password,$Storedpassword)){
+                $data['result'] = "log in";
+                $data['status'] = "great";
+                $_SESSION['userid'] = $userid;
 
-        //header("location : check.php");
-        echo json_encode( $data );
+                //header("location : check.php");
+                echo json_encode( $data );
+                die;
+            }else{
+                $data['result'] = "incorrect password";
+                $data['status'] = "bad";
+
+                echo json_encode( $data );
+                die;
+            }
+        }
     }
     else{
-        $data['result'] = "incorrect email or password";
+        $data['result'] = "incorrect email";
         $data['status'] = "bad";
 
         echo json_encode( $data );
+        die;
     }
 
 }
