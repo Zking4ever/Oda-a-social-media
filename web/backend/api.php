2a2e4f7920eaa52ceffc50592ad868eb65cfde8c
Astawus <yanosastawus@gmail.com>
2025-08-15 15:11:25 +0300

    For faster access swithed the home page to html file and handling all process in the backend

--- web/backend/api.php
+++ web/backend/api.php
@@ -3,14 +3,13 @@
 require "conn.php";
 session_start();
 
-/*
 if($_SESSION['userid'] =='' || !isset($_SESSION['userid'])){
     session_destroy();
+    echo "you are not logged in. Please log in to access this page.";
     die;
 }
 $userid = $_SESSION['userid'];
 
-    */
 if(isset($_GET['request_type']) && $_GET['request_type']=="loadHome"){
 
     include "includes/loadHome.php";
